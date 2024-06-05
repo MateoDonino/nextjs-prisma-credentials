a0ca4357be8e9c690518b8205da0ad5f1c3aa320
mateodonino <mateodonino@gmail.com>
2024-06-05 18:43:30 -0300

    v1.2

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -27,78 +27,85 @@ function RegisterPage() {
         <h1 className="text-white font-semibold text-4xl mb-4 text-center">
           Registe
         </h1>
-        <label htmlFor="username" className="text-gray-500 mb-2 block text-sm">
-          Username
-        </label>
-        <input
-          type="text"
-          {...register("username", {
-            required: { value: true, message: "Usernameis Required" },
-          })}
-          className="p-3 rounded block mb-2 bg-slate-900 text-slate-500 w-full"
-          placeholder="Your Username"
-        />
-        {errors.username && (
-          <span className="text-red-500 text-sm">
-            {errors.username.message}
-          </span>
-        )}
-        <label htmlFor="email" className="text-gray-500  mb-2 block text-sm">
-          Email
-        </label>
-        <input
-          type="email"
-          {...register("email", {
-            required: { value: true, message: "Email is Required" },
-          })}
-          className="p-3 rounded block mb-2 bg-slate-900 text-slate-500 w-full"
-          placeholder="Your Email"
-        />
-        {errors.email && (
-          <span className="text-red-500 text-sm">{errors.email.message}</span>
-        )}
-        <label
-          htmlFor="password"
-          className=" text-gray-500  mb-2 block text-sm"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          {...register("password", {
-            required: { value: true, message: "Password is Required" },
-          })}
-          className="p-3 rounded block mb-2 bg-slate-900 text-slate-500 w-full"
-          placeholder="********"
-        />
-        {errors.password && (
-          <span className="text-red-500 text-sm">
-            {errors.password.message}
-          </span>
-        )}
-        <label
-          htmlFor="confirmPassword"
-          className="text-gray-500  mb-2 block text-sm"
-        >
-          Confirm Password
-        </label>
-        <input
-          type="password"
-          {...register("confirmPassword", {
-            required: { value: true, message: "Confirm Password is Required" },
-          })}
-          className="p-3 rounded block mb-2 bg-slate-900 text-slate-500 w-full"
-          placeholder="********"
-        />
-
-        {errors.confirmPassword && (
-          <span className="text-red-500 text-sm">
-            {errors.confirmPassword.message}
-          </span>
-        )}
-        <button className="mt-2 w-full bg-blue-700 hover:bg-blue-800 text-white p-3 rounded-lg">
-          Register
-        </button>
+        <div className="border-2 shadow-2xl border-gray-900 rounded-2xl p-4 ">
+          <label
+            htmlFor="username"
+            className="text-gray-500 mb-2 block text-sm"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            {...register("username", {
+              required: { value: true, message: "Usernameis Required" },
+            })}
+            className="p-3 rounded block mb-2 bg-slate-900 text-slate-500 w-full"
+            placeholder="Your Username"
+          />
+          {errors.username && (
+            <span className="text-red-500 text-sm">
+              {errors.username.message}
+            </span>
+          )}
+          <label htmlFor="email" className="text-gray-500  mb-2 block text-sm">
+            Email
+          </label>
+          <input
+            type="email"
+            {...register("email", {
+              required: { value: true, message: "Email is Required" },
+            })}
+            className="p-3 rounded block mb-2 bg-slate-900 text-slate-500 w-full"
+            placeholder="Your Email"
+          />
+          {errors.email && (
+            <span className="text-red-500 text-sm">{errors.email.message}</span>
+          )}
+          <label
+            htmlFor="password"
+            className=" text-gray-500  mb-2 block text-sm"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            {...register("password", {
+              required: { value: true, message: "Password is Required" },
+            })}
+            className="p-3 rounded block mb-2 bg-slate-900 text-slate-500 w-full"
+            placeholder="********"
+          />
+          {errors.password && (
+            <span className="text-red-500 text-sm">
+              {errors.password.message}
+            </span>
+          )}
+          <label
+            htmlFor="confirmPassword"
+            className="text-gray-500  mb-2 block text-sm"
+          >
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            {...register("confirmPassword", {
+              required: {
+                value: true,
+                message: "Confirm Password is Required",
+              },
+            })}
+            className="p-3 rounded block mb-2 bg-slate-900 text-slate-500 w-full"
+            placeholder="********"
+          />
+          {errors.confirmPassword && (
+            <span className="text-red-500 text-sm">
+              {errors.confirmPassword.message}
+            </span>
+          )}
+          <button className="mt-2 w-full bg-blue-700 hover:bg-blue-800 text-white p-3 rounded-lg">
+            Register
+          </button>
+        </div>
       </form>
     </div>
   );
